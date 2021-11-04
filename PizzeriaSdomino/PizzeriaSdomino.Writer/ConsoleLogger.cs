ef3b4175df0cc4afae9f13b05a3b382cee12632d
Akulnaij91 <kennet_91@hotmail.it>
2021-11-04 17:46:50 +0100

    test

--- PizzeriaSdomino/PizzeriaSdomino.Writer/ConsoleLogger.cs
+++ PizzeriaSdomino/PizzeriaSdomino.Writer/ConsoleLogger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PizzeriaSdomino.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace PizzeriaSdomino.Writer
 {
-    class AuditConsoleLog
+    public class ConsoleLogger : Audit
     {
+        public override void Log(Scontrino ordine)
+        {
+            
+            Console.WriteLine($"Scontrino numero {ordine.idScontrino} - Elenco pizze: {String.Concat(ordine.listaPizze.Select(x=>x.basePizza), " ", ordine.listaPizze.Select(x => x.impastoPizza), " ", String.Concat(",",ordine.listaPizze.Select(x=>x.aggiuntePizza.Select(y=>y.descrizione))))}- Totale: {CalcolaPrezzo(ordine)}");
+        }
+
+        public decimal CalcolaPrezzo(Scontrino ordine)
+        {
+            if (ordine.listaPizze.Select(x => x.aggiuntePizza.Any(y => y.descrizione.Equals("Ananas"))))
+                return 0;
+
+            
+        }
     }
 }
